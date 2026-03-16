7c0c2d795cc78c5c526e30cdf4162e03052a9302
Guillem Serra <guillemserralorenz@gmail.com>
2026-03-16 12:10:13 +0100

    material init

--- src/graphics/materials/Material.h
+++ src/graphics/materials/Material.h
@@ -1,20 +1,26 @@
 ﻿// Copyright (c) Guillem Serra. All Rights Reserved.
 
 #pragma once
+#include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 
 struct Material
 {
     glm::vec4 Albedo = glm::vec4(1.0f);
     float Metallic = 0.0f;
-    float Roughness = 1.f;
+    float Roughness = 0.5f;
     
-    float Specular;
-    float RefractionIndex;
-    float AO;
-    float Emission;
-    float Transparency;
-    float Fresnel;
-    float IOR;
-    float Dissolve;
+    float Specular = 0.35f;
+    float Reflectivity = 0.0f;
+    float RefractionIndex = 1.0f;
+    float AO = 1.0f;
+    float Emission = 0.0f;
+    float Transparency = 0.0f;
+    float Fresnel = 0.04f;
+    float IOR = 1.0f;
+    float Dissolve = 1.0f;
+
+    glm::vec3 Absorption = glm::vec3(0.15f);
+    float Scattering = 0.0f;
+    float AcousticReflectivity = 0.0f;
 };
