dbe5ddcb4ea13342574c02a0edf6c7857ba627d2
Guillem Serra <guillemserralorenz@gmail.com>
2026-09-12 07:48:32 +0200

    Set runtime directory using SDL_GetBasePath and std::filesystem for asset management

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,29 @@
 
 #include "RayTracingGame.h"
 
+#include <filesystem>
+
 #include "core/App.h"
 
 using namespace SerraEngine;
 
 int main(int argc, char* argv[])
 {
+    const char* basePath = SDL_GetBasePath();
+    if (!basePath)
+    {
+        SDL_Log("Failed to locate runtime assets: %s", SDL_GetError());
+        return 1;
+    }
+
+    std::error_code pathError;
+    std::filesystem::current_path(basePath, pathError);
+    if (pathError)
+    {
+        SDL_Log("Failed to use runtime directory '%s': %s", basePath, pathError.message().c_str());
+        return 1;
+    }
+
     App EngineApp = App(argc, argv);
     if (!EngineApp.Init("Raytracing + audio", 960, 600))
     {
