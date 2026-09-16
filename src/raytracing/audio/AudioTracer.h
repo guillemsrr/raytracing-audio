3e7bc3a53540a00a44bf0a9667bcf2e8b5b94196
Guillem Serra <guillemserralorenz@gmail.com>
2026-09-16 12:00:44 +0200

    audio tracer basic step by step implementation

--- src/raytracing/audio/AudioTracer.h
+++ src/raytracing/audio/AudioTracer.h
@@ -3,9 +3,9 @@
 #pragma once
 
 #include "../base/RaytracerBase.h"
-#include "CandidateHit.h"
-
 #include "audio/AudioEmitter.h"
+
+class AudioListener;
 
 class AudioTracer : public RaytracerBase
 {
@@ -16,7 +16,19 @@ public:
 
 protected:
     color TraceRay(const Ray& ray) const override;
+    //TODO: sound doesn't do color
+    void TracePath(AudioEmitter* emitter, vec3 listenerPosition);
 
 private:
     const AudioEmitter* _audioEmitter = nullptr;
+    int _maxBounces = 10;
+    int _maxSoundDistance = 100000;
+    
+    float _binDuration = 0.001f;         // each bin covers 1 ms of arrival time                                                                                                                                                                                                                                    
+    float _maxTime     = 2.0f;           // ignore anything arriving after 2 s  
+    
+    //For impulse responses, analogue of _colorBuffer
+    std::vector<glm::vec3> _histogram;   // one vec3 (3 bands) of energy per bin
+    
+    AudioListener* listener;
 };
